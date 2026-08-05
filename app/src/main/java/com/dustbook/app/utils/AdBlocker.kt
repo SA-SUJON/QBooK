89d1ebf55df23e6f89864cd0a8a60885f6b84626
Dustbook Build <build@dustbook.app>
2026-08-05 18:29:42 +0000

    Revert "reels scrolling fix"
    This reverts commit 19280ee34cad6ad67fba67f25063e28b8bb1aca5.

--- app/src/main/java/com/dustbook/app/utils/AdBlocker.kt
+++ app/src/main/java/com/dustbook/app/utils/AdBlocker.kt
@@ -713,13 +713,7 @@ object AdBlocker {
                     for (var i = 0; i < vs.length; i++) {
                       vs[i].pause();
                       vs[i].removeAttribute('src');
-                      // load() after removeAttribute('src') sends Chromium's
-                      // media pipeline through resource selection, emptied,
-                      // abort, then error recovery with no source - all on
-                      // the main thread. That is the 1-2s freeze/black
-                      // screen. pause() + removeAttribute already stop the
-                      // buffer and release the resource; load() adds nothing
-                      // and was the actual cause of the hang.
+                      vs[i].load();
                     }
                   } catch (e) {}
                 }
