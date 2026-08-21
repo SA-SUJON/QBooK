1fbf20875842068eedd7a5e3338626140291129d
SA-SUJON <1.11200248e+08+SA-SUJON@users.noreply.github.com>
2026-08-21 16:52:12 +0000

    Release QBooK 6.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "org.qbook"
         minSdk = 26
         targetSdk = 34
-        versionCode = 136
-        versionName = "5.2.26"
+        versionCode = 600
+        versionName = "6.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
